c235b8c3533c267927829603ae3fcbfa693b603c
Daniel Vasić <dvasic1@gmail.com>
2024-10-23 10:55:46 +0200

    Druge vježbe

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import React, {useState} from "react";
 import {View, TextInput, Button, Text} from "react-native";
 import {signInWithEmailAndPassword} from "firebase/auth";
-import {auth} from "./firebaseConfig";
+import {auth, firestore} from "./firebaseConfig";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
 export default function App() {
   const [email, setEmail] = useState("");
   const [passw, setPassw] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [loggedIn, setLoggedIn] = useState(false); 
+  const [profile, setProfile] = useState({name:"", age:"", bio:""});
 
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, email, passw).then(
-      () => {
+      async (userCredentials) => {
         setLoggedIn(true);
+        const userId = userCredentials.user.uid;
+        const docRef = doc(firestore, "users", userId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setProfile(docSnap.data())
+        } else {
+          await setDoc(doc(firestore, "users", userId), {
+            name:"", age:"", bio:""
+          });
+        }
       }
     ).catch((error) => setErrorMsg(error.message));
+  };
+
+  const handleSaveProfile = async () => {
+    const userId = auth.currentUser.uid;
+    await setDoc(doc(firestore, "users", userId), profile);
   };
 
   if (loggedIn) {
@@ -22,6 +40,25 @@ export default function App() {
       <View>
         <Text>Dobrodošli na sustav</Text>
         <Button title="Odjavi se"></Button>
+        <TextInput 
+          placeholder="Unesite svoje ime"
+          value={profile.name}
+          onChangeText={(text) => setProfile({...profile, name: text})}
+          />
+
+        <TextInput 
+          placeholder="Unesite svoje godine"
+          value={profile.age}
+          onChangeText={(text) => setProfile({...profile, age: text})}
+          />
+
+        <TextInput 
+          placeholder="O meni ..."
+          value={profile.bio}
+          onChangeText={(text) => setProfile({...profile, bio: text})}
+          />
+
+        <Button title="Spremi profil" onPress={handleSaveProfile} />
       </View>
     );
   }
