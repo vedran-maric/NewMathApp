09f0c2a50eda801e770202bba3581fc7fa93874f
Vedran Marić <44380214+vedran-maric@users.noreply.github.com>
2024-11-27 22:00:31 +0100

    Update firebaseConfig.js
    Uklonjeni API-ji zbog sigurnosti

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgeUjz-Ng1Dk-b3uZaGumx2RLsCrcDGs0",
-    authDomain: "mathapp-cd750.firebaseapp.com",
-    projectId: "mathapp-cd750",
-    storageBucket: "mathapp-cd750.firebasestorage.app",
-    messagingSenderId: "1078629689647",
-    appId: "1:1078629689647:web:6fde3a31e9c6aef6cf99da",
-    measurementId: "G-78PMW27WQQ"
+    
 };
 
 const app = initializeApp(firebaseConfig);
